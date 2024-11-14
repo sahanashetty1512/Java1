ad879349348ec7c0cd6b71b2bd70705030e5f7ef
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2024-11-14 10:34:07 +0530

    Add files via upload

---
+++ TestTools.java
@@ -0,0 +1,12 @@
+package pkg1;
+public class TestTools{
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        t1.numbers(10);
+        t1.even(10);
+        t1.odd(10);
+        t1.multi3(10);
+        t1.multi4(10);
+        t1.multi5(10);    
+    }
+}
---
+++ Tools.java
@@ -0,0 +1,43 @@
+package pkg1;
+class Tools{
+    public void numbers(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(i+"");
+        }
+        System.out.println();
+    }
+    public void even(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(2*i+" ");
+        }
+        System.out.println();
+    }
+    public void odd(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(((2*i)+1)+" ");
+        }
+        System.out.println();
+    }
+    public void multi3(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(3*i+" ");
+        }
+        System.out.println();
+    }
+    public void multi4(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(4*i+" ");
+        }
+        System.out.println();
+    }
+    
+    public void multi5(int n1){
+        for(int i=0;i<n1;i++){
+            System.out.print(5*i+" ");
+        }
+        System.out.println();
+    }
+}
+    
+    
+    
