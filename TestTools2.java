fb1a210e8adbe2c8c7bf4c8e26136cce6720e329
sahanashetty1512 <sahana.23cs128@sode-edu.in>
2024-11-21 09:22:43 +0530

    Add files via upload

---
+++ TestTools2.java
@@ -0,0 +1,13 @@
+package pkg2;
+import pkg1.Tools;
+public class TestTools2 {
+   public static void main(String[] args){
+        Tools t1=new Tools();
+        t1.numbers(10);
+        t1.even(10);
+        t1.odd(10);
+        t1.multi3(10);
+        t1.multi4(10);
+        t1.multi5(10);    
+    }
+}
